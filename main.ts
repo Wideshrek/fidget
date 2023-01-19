d57837467ca1e25643d8a486ed2ffb04d9376995
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-19 12:15:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Magic Eight MicroBit")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
