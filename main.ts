223181428ac2d4f84413845b56aa8148dad2194c
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-20 12:00:55 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let m8b = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showString("Magic Eight MicroBit")
 })
